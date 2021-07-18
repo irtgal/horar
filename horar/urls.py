1a71745a8c80753eea2f7dcc0494603c7dcd931a
irgal <gal.irt.01@gmail.com>
2021-07-18 21:57:36 +0200

    ^/$

--- horar/urls.py
+++ horar/urls.py
@@ -5,8 +5,11 @@ from .views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
+    
     
     url('administrator/', include('administrator.urls')),
     url('urniki/', include('timetables.urls')),
+    url(r'^$', home, name="home"),
 	url(r'^doma/', home, name="home"),
 ]
